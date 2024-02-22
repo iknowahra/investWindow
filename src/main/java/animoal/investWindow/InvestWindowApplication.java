57c8bbcfc21bede8894b926c008effc1284b40b6
iknowahra <iknowahrajoh@gmail.co.kr>
2024-02-22 12:36:37 +0900

    🚀Start project

---
+++ src/main/java/animoal/investWindow/InvestWindowApplication.java
@@ -0,0 +1,13 @@
+package animoal.investWindow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestWindowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestWindowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/animoal/investWindow/InvestWindowApplicationTests.java
@@ -0,0 +1,13 @@
+package animoal.investWindow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestWindowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
